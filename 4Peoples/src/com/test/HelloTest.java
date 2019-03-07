045cc802af5d40e9ff190eb673275944cb054418
ksm512 <ksm512@DESKTOP-3FEI4IQ>
2019-03-07 13:04:32 +0900

    vv

--- 4Peoples/src/com/test/HelloTest.java
+++ 4Peoples/src/com/test/HelloTest.java
@@ -3,6 +3,6 @@ package com.test;
 public class HelloTest {
 	public static void main(String args[]) {
 		System.out.println("hi지유니지유니");
-		System.out.println("윤정윤정dsdaasd");
+		System.out.println("윤정윤정dsdaasddsasd");
 	}
 }
